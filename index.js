7119a6b93de5e7113851c46863dfdc1f9f063437
Tawsif Adnan <tawsif.adnan22@gmail.com>
2023-07-08 21:25:23 +0600

    add contact API created

--- index.js
+++ index.js
@@ -8,6 +8,28 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+// data
+const contacts = require("./data/contacts.json");
+
+// get all contacts
+app.get("/contacts", (req, res) => {
+  res.send(contacts);
+});
+
+// add contact API
+app.post("/contacts", (req, res) => {
+  const contact = req.body;
+  const query = { number: contact.number };
+  const existingContact = contacts.find(query);
+
+  if (existingContact) {
+    return res.send("Contact already exists");
+  }
+
+  const result = contacts.push(contact);
+  res.send(result);
+});
+
 app.get("/", (req, res) => {
   res.send("contact management server running");
 });
