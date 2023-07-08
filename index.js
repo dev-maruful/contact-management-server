4faf6dacba7f3b94260c37d83cc7eff4c0ea65a6
Tawsif Adnan <tawsif.adnan22@gmail.com>
2023-07-09 01:38:55 +0600

    delete contact api created

--- index.js
+++ index.js
@@ -73,6 +73,14 @@ async function run() {
       res.send(result);
     });
 
+    // delete contact
+    app.delete("/contacts/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await contactsCollection.deleteOne(query);
+      res.json(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
